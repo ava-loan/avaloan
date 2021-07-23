9bcc12f31436b7142b9514934d7ce133cd2fc7c6
ppedziwiatr <peter@redstone.finance>
2021-07-23 15:18:18 +0200

    out of gas fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,9 +5,13 @@ import "hardhat-watcher";
 
 export default {
   solidity: "0.8.2",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 7
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000
     }
   },
   paths: {
