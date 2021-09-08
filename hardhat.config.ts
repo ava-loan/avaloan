749f9494105930ea2f0aaa09f8205ac3bc199c24
kamilovsky <muca.kamil@gmail.com>
2021-09-08 22:04:43 +0200

    Fork the Avalanche mainnet while running tests
    * Fork the Avalanche mainnet while running tests
    * blockNumber option iscurrently unsupported for non-Ethereum networks
      as confirmed by the hardhat team. They are planning to implement
      this in the future so that we can leverage the caching mechanism.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,11 @@ export default {
       gas: 12000000,
       blockGasLimit: 0x1fffffffffffff,
       allowUnlimitedContractSize: true,
-      timeout: 1800000
+      timeout: 1800000,
+      forking: {
+        url: 'https://api.avax.network/ext/bc/C/rpc',
+        // blockNumber: XXXXXXX - currently unsupported for non-Ethereum networks as confirmed by the hardhat team. They are planning to implement this in the future so that we can leverage the caching mechanism.
+      }
     }
   },
   paths: {
